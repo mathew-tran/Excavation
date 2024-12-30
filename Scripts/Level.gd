a313f28cb5614363965906d2a94caf6c7ef29190
Mathew Tran <mathew.tran.dev@gmail.com>
2024-12-30 12:16:35 -0800

    play with lighting to try to give more atmosphere

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+@export var LightColor : LightResource
+
 func _ready():
+	var colorMod = CanvasModulate.new()
+	add_child(colorMod)
+	colorMod.color = LightColor.LightColor
+	
 	await get_tree().create_timer(.2).timeout
 	Transition.FadeOut()
